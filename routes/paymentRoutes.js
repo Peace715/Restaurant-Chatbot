5656fc98cce80cf8ffb96e9961fdcfdb622f8a5e
Peace715 <peaceotoide78@gmail.com>
2026-08-08 08:43:58 +0100

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { initializePayment } = require("../controllers/paymentController");
-const { verifyPayment } = require("../controllers/paymentController");
-
-router.post("/pay", initializePayment);
-router.get("/verify/:reference", verifyPayment);
-
-module.exports = router;
