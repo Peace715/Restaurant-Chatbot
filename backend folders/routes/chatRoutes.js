faa09f98f9d86d58f0d6c289a89a93451166ea9e
Peace715 <peaceotoide78@gmail.com>
2026-08-08 09:57:41 +0100

    Update chatRoutes.js

--- backend folders/routes/chatRoutes.js
+++ backend folders/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { handleChatbot } = require('../backend/controllers/chatbotController');
+const { handleChatbot } = require('../controllers/chatbotController');
 
 router.post('/', async (req, res) => {
   const { message, deviceId } = req.body;
